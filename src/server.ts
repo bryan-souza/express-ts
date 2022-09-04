28f5b2d9d060c2a4db447e393c7635c182b2186e
bryan-souza <bryans.souza@protonmail.com>
2022-09-04 10:47:30 -0300

    feat: added middlewares

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import indexRouter from './routers/index.router';
 
 const app = express();
+
+app.use(express.json()); // JSON parser
+app.use(express.urlencoded({ extended: true })); // Form-Data parser
 app.use('/', indexRouter);
 
 app.listen(3333, () => 'Server running on port 3333');
